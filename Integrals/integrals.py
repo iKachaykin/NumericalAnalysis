224224fe9ebdd4a45a5baa0bd6911ea8515a12f4
iKachaykin <ivankachaykin707@gmail.com>
2017-10-05 23:36:00 +0300

    Integrals first implementation
    First implementation of algorithms, that allow to find the definite integral value.

---
+++ Integrals/integrals.py
@@ -0,0 +1,141 @@
+import numpy as np
+import numpy.polynomial.legendre as leg
+import scipy.interpolate as interp
+
+
+def left_rectangles(integrand, left_b_arr, right_b_arr, N=10):
+    if not isinstance(left_b_arr, np.ndarray) or not isinstance(right_b_arr, np.ndarray):
+        left_b_arr, right_b_arr = np.array(left_b_arr), np.array(right_b_arr)
+    if left_b_arr.ravel().size != right_b_arr.ravel().size:
+        return None
+    results = []
+    for left, right in zip(left_b_arr.ravel(), right_b_arr.ravel()):
+        weight = 1
+        if left >= right:
+            left, right = right, left
+            weight = -1
+        x_vals = np.linspace(left, right, N + 1)
+        result = integrand(x_vals[:x_vals.size - 1]).sum() * (right - left) / N
+        results.append(result * weight)
+    return np.array(results).reshape(left_b_arr.shape)
+
+
+def right_rectangles(integrand, left_b_arr, right_b_arr, N=10):
+    if not isinstance(left_b_arr, np.ndarray) or not isinstance(right_b_arr, np.ndarray):
+        left_b_arr, right_b_arr = np.array(left_b_arr), np.array(right_b_arr)
+    if left_b_arr.ravel().size != right_b_arr.ravel().size:
+        return None
+    results = []
+    for left, right in zip(left_b_arr.ravel(), right_b_arr.ravel()):
+        weight = 1
+        if left >= right:
+            left, right = right, left
+            weight = -1
+        x_vals = np.linspace(left, right, N + 1)
+        result = integrand(x_vals[1:]).sum() * (right - left) / N
+        results.append(result * weight)
+    return np.array(results).reshape(left_b_arr.shape)
+
+
+def central_rectangles(integrand, left_b_arr, right_b_arr, N=10):
+    if not isinstance(left_b_arr, np.ndarray) or not isinstance(right_b_arr, np.ndarray):
+        left_b_arr, right_b_arr = np.array(left_b_arr), np.array(right_b_arr)
+    if left_b_arr.ravel().size != right_b_arr.ravel().size:
+        return None
+    results = []
+    for left, right in zip(left_b_arr.ravel(), right_b_arr.ravel()):
+        weight = 1
+        if left >= right:
+            left, right = right, left
+            weight = -1
+        x_vals = np.linspace(left, right, N + 1)
+        result = integrand((x_vals[1:] + x_vals[:x_vals.size - 1]) / 2).sum() * (right - left) / N
+        results.append(result * weight)
+    return np.array(results).reshape(left_b_arr.shape)
+
+
+def trapezoid(integrand, left_b_arr, right_b_arr, N=10):
+    if not isinstance(left_b_arr, np.ndarray) or not isinstance(right_b_arr, np.ndarray):
+        left_b_arr, right_b_arr = np.array(left_b_arr), np.array(right_b_arr)
+    if left_b_arr.ravel().size != right_b_arr.ravel().size:
+        return None
+    results = []
+    for left, right in zip(left_b_arr.ravel(), right_b_arr.ravel()):
+        weight = 1
+        if left >= right:
+            left, right = right, left
+            weight = -1
+        x_vals = np.linspace(left, right, N + 1)
+        result = (integrand(x_vals[1:x_vals.size - 1]).sum() + (integrand(x_vals[0]) + integrand(x_vals[x_vals.size - 1])) / 2) * (right - left) / N
+        results.append(result * weight)
+    return np.array(results).reshape(left_b_arr.shape)
+
+
+def Simpson(integrand, left_b_arr, right_b_arr, N=10):
+    if not isinstance(left_b_arr, np.ndarray) or not isinstance(right_b_arr, np.ndarray):
+        left_b_arr, right_b_arr = np.array(left_b_arr), np.array(right_b_arr)
+    if left_b_arr.ravel().size != right_b_arr.ravel().size:
+        return None
+    results = []
+    for left, right in zip(left_b_arr.ravel(), right_b_arr.ravel()):
+        weight = 1
+        if left >= right:
+            left, right = right, left
+            weight = -1
+        x_vals = np.linspace(left, right, 2 * N + 1)
+        result = (2 * integrand(x_vals[2:2 * N - 1:2]).sum() + integrand(left) + integrand(right) + 4 * integrand(x_vals[1::2]).sum())\
+                 * (right - left) / N / 6
+        results.append(result * weight)
+    return np.array(results).reshape(left_b_arr.shape)
+
+
+def Gaussian(integrand, left_b_arr, right_b_arr, N=10):
+    if not isinstance(left_b_arr, np.ndarray) or not isinstance(right_b_arr, np.ndarray):
+        left_b_arr, right_b_arr = np.array(left_b_arr), np.array(right_b_arr)
+    if left_b_arr.ravel().size != right_b_arr.ravel().size:
+        return None
+    if N > 100 or N < 0:
+        N = 10
+    results = []
+    legcoeffs = np.where(np.arange(N + 2) == N + 1, 1, 0)
+    x_vals = leg.legroots(legcoeffs)
+    weight_vals = 2 / (1 - x_vals**2) / (leg.legval(x_vals, leg.legder(legcoeffs)) ** 2)
+    for x in x_vals:
+        print(np.str(x) + '\n')
+    for left, right in zip(left_b_arr.ravel(), right_b_arr.ravel()):
+        weight = 1
+        if left >= right:
+            weight = -1
+            left, right = right, left
+        result = (right - left) / 2 * integrand((right - left) / 2 * x_vals + (right + left) / 2).dot(weight_vals)
+        results.append(result * weight)
+    return np.array(results).reshape(left_b_arr.shape)
+
+
+def create_approximating_figure_left_rectangles(integrand, nodes, current_i, dot_num, N=10):
+    return integrand(nodes[current_i]) * np.ones(dot_num)
+
+
+def create_approximating_figure_right_rectangles(integrand, nodes, current_i, dot_num, N=10):
+    return integrand(nodes[current_i + 1]) * np.ones(dot_num)
+
+
+def create_approximating_figure_central_rectangles(integrand, nodes, current_i, dot_num, N=10):
+    return integrand((nodes[current_i] + nodes[current_i + 1]) / 2) * np.ones(dot_num)
+
+
+def create_approximating_figure_trapeze(integrand, nodes, current_i, dot_num, N=10):
+    return ((np.linspace(nodes[current_i], nodes[current_i + 1], dot_num) - nodes[current_i]) /
+    (nodes[current_i + 1] - nodes[current_i])) * (integrand(nodes[current_i + 1]) - integrand(nodes[current_i])) + (
+        integrand(nodes[current_i]))
+
+
+def create_approximating_figure_Simpson(integrand, nodes, current_i, dot_num, N=10):
+    lagrange_nodes = np.array([nodes[current_i], (nodes[current_i] + nodes[current_i + 1]) / 2, nodes[current_i + 1]])
+    lagrange_poly = interp.lagrange(lagrange_nodes, integrand(lagrange_nodes))
+    return lagrange_poly(np.linspace(nodes[current_i],
+            nodes[current_i + 1], dot_num))
+
+
+def Gaussian_node_vals(N=10):
+    return leg.legroots(np.where(np.arange(N + 2) == N + 1, 1, 0))
---
+++ Integrals/main_1.py
@@ -0,0 +1,93 @@
+from integrals import *
+import numpy as np
+import matplotlib.pyplot as plt
+import numpy.polynomial.legendre as leg
+
+
+def f(x):
+    return 1 / (4 * np.exp(x) + 9 * np.exp(-x))
+
+
+def norm_f(f, x, a, b):
+    return (b - a) / 2 * f((b - a) / 2 * x + (b + a) / 2)
+
+
+col_labels = ("Метод", "Выч. значение", "Точное значение", "Абс. погрешность", "Оценка погрешности")
+methods_names = ("Левых пр.", "Правых пр.", "Центральных пр.",
+                 "Трапеций", "Симпсона", "Гаусса")
+funcs_create_figs = (create_approximating_figure_left_rectangles, create_approximating_figure_right_rectangles,
+                     create_approximating_figure_central_rectangles, create_approximating_figure_trapeze,
+                     create_approximating_figure_Simpson)
+interval_num, left_border, right_border, b_interval_dot_num, s_interval_dot_num, additional_interval_koeff = \
+    10, -1, 1, 500, 100, 0
+index_Gaussian = 5
+exact_val = np.float((np.arctan(2 * np.exp(right_border) / 3) - np.arctan(2 * np.exp(left_border) / 3)) / 6)
+x_vals = np.linspace(left_border - additional_interval_koeff * (right_border - left_border),
+                     right_border + additional_interval_koeff * (right_border - left_border), b_interval_dot_num)
+x_node_vals = np.linspace(left_border, right_border, interval_num + 1)
+f_vals = np.arange(100)
+y_vals_left = \
+np.where(np.sign(f(left_border)) * np.linspace(0, np.sign(f(left_border)) * np.abs(f(x_vals)).max(), x_vals.size) <
+         np.sign(f(left_border)) * f(left_border),
+         np.linspace(0,  np.sign(f(left_border)) * np.abs(f(x_vals)).max(), x_vals.size), 0)
+y_vals_right = \
+np.where(np.sign(f(right_border)) * np.linspace(0, np.sign(f(right_border)) * np.abs(f(x_vals)).max(), x_vals.size) <
+         np.sign(f(right_border)) * f(right_border),
+         np.linspace(0, np.sign(f(right_border)) * np.abs(f(x_vals)).max(), x_vals.size), 0)
+y_vals_left_in_Gaussian = \
+np.where(np.sign(norm_f(f, -1, left_border, right_border)) *
+         np.linspace(0, np.sign(norm_f(f, -1, left_border, right_border)) * np.abs(norm_f(f, x_vals, left_border, right_border)).max(), x_vals.size) <
+         np.sign(norm_f(f, -1, left_border, right_border)) * norm_f(f, -1, left_border, right_border),
+         np.linspace(0, np.sign(norm_f(f, -1, left_border, right_border)) * np.abs(norm_f(f, x_vals, left_border, right_border)).max(), x_vals.size), 0)
+y_vals_right_in_Gaussian = \
+np.where(np.sign(norm_f(f, 1, left_border, right_border)) *
+         np.linspace(0, np.sign(norm_f(f, 1, left_border, right_border)) * np.abs(norm_f(f, x_vals, left_border, right_border)).max(), x_vals.size) <
+         np.sign(norm_f(f, 1, left_border, right_border)) * norm_f(f, 1, left_border, right_border),
+         np.linspace(0, np.sign(norm_f(f, 1, left_border, right_border)) * np.abs(norm_f(f, x_vals, left_border, right_border)).max(), x_vals.size), 0)
+result_values = (left_rectangles(f, left_border, right_border, interval_num),
+                 right_rectangles(f, left_border, right_border, interval_num),
+                 central_rectangles(f, left_border, right_border, interval_num),
+                 trapezoid(f, left_border, right_border, interval_num),
+                 Simpson(f, left_border, right_border, interval_num),
+                 Gaussian(f, left_border, right_border, interval_num))
+data = []
+for result, name in zip(result_values, methods_names):
+    data.append([name, result, exact_val, np.abs(result - exact_val)])
+figures = []
+for i in range(len(methods_names)):
+    figures.append(plt.figure(figsize=(12, 7)))
+    figures[i].suptitle("Метод " + methods_names[i])
+figures.append(plt.figure(figsize=(12, 7)))
+figures[len(methods_names)].suptitle("Таблица с результатами вычислений")
+for i in range(len(methods_names)):
+    plt.figure(i + 1)
+    ax = plt.subplot()
+    ax.fill_between(np.linspace(left_border, right_border, b_interval_dot_num), np.zeros(b_interval_dot_num),
+                    f(np.linspace(left_border, right_border, b_interval_dot_num)), color='blue', alpha=0.3)
+    for j in range(x_node_vals.size - 1):
+        if i == index_Gaussian:
+            break
+        f_vals = funcs_create_figs[i](f, x_node_vals, j, s_interval_dot_num, interval_num)
+        ax.fill_between(np.linspace(x_node_vals[j], x_node_vals[j + 1], s_interval_dot_num),
+                        np.zeros(s_interval_dot_num), f_vals, color='red', alpha=0.1)
+    ax.grid(True, zorder=5)
+    if i == index_Gaussian:
+        x_node_vals = Gaussian_node_vals(interval_num)
+    plt.plot(x_vals, f(x_vals), 'r-', np.ones_like(x_vals) * left_border, y_vals_left, 'k-',
+             np.ones_like(x_vals) * right_border, y_vals_right, 'k-', x_vals, np.zeros_like(x_vals), 'k-',
+             x_node_vals, np.zeros_like(x_node_vals), 'k|')
+plt.plot(x_vals, norm_f(f, x_vals, left_border, right_border), 'b-', np.ones_like(x_vals) * -1, y_vals_left_in_Gaussian,
+         'c-', np.ones_like(x_vals), y_vals_right_in_Gaussian, 'c-')
+ax.fill_between(np.linspace(-1, 1, b_interval_dot_num), np.zeros(b_interval_dot_num),
+                norm_f(f, np.linspace(-1, 1, b_interval_dot_num), left_border, right_border), color='magenta',
+                alpha=0.1)
+plt.plot(x_vals, leg.legval(x_vals, np.where(np.arange(interval_num + 2) == interval_num + 1, 1, 0)), 'k-')
+# plt.axis([x_vals.min(), x_vals.max(),
+#           np.min([f(x_vals).min(), norm_f(f, x_vals, left_border, right_border).min()]),
+#           np.max([f(x_vals).max(), norm_f(f, x_vals, left_border, right_border).max()])])
+plt.figure(7)
+plt.axis('off')
+ax = plt.subplot()
+tab = plt.table(cellText=data, loc='center', colLabels=col_labels, cellLoc='center')
+plt.show()
+plt.close()
