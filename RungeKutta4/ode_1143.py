ea82953e97ef529911effc6e7cc67d4ada34f9da
iKachaykin <ivankachaykin707@gmail.com>
2019-02-27 21:48:30 +0200

    Implementetion of RK4
    Implementetion of fourth order Runge-Kutta method via Python and Cython that allow to solve systems of ODE with given initial conditions. Some examples are included.

---
+++ RungeKutta4/ode_1143.py
@@ -0,0 +1,47 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import RungeKutta4 as rk4
+import RungeKutta4_cython as rk4_cython
+import time as time
+
+
+def ode_f(t, x):
+    return np.array([x[1] / t,
+                     x[1] * (x[0] + 2.0 * x[1] - 1.0) / t / (x[0] - 1.0)])
+
+
+def ode_exact_solution(t):
+    return np.array([(t + 2.0) / (t + 1.0),
+                     -t / (t + 1.0) / (t + 1.0)])
+
+
+if __name__ == '__main__':
+
+    a, b = 0.5, 5.0
+    grid_dot_num = 500
+    figsize = (15.0, 7.5)
+    t0, x0 = 1.0, np.array([1.5, -0.25])
+    h_initial = 0.1
+
+    t_exact = np.linspace(a, b, grid_dot_num)
+    x_exact = ode_exact_solution(t_exact)
+
+    tp = time.time()
+    t_numerical, x_numerical = rk4.solve_ode(t0, x0, a, b, ode_f, calc_eps=1e-8, h_initial=h_initial)
+    tp = time.time() - tp
+
+    tc = time.time()
+    t_numerical_cython, x_numerical_cython = rk4_cython.solve_ode(t0, x0, a, b, ode_f, calc_eps=1e-8,
+                                                                  h_initial=h_initial)
+    tc = time.time() - tc
+
+    print('||t1 - t2|| = %f\n||x1 - x2|| = %f\nTime python: %f\nTime cython: %f\n' %
+          (np.linalg.norm(t_numerical - t_numerical_cython), np.linalg.norm(x_numerical - x_numerical_cython),
+           tp, tc))
+
+    plt.figure(figsize=figsize)
+    plt.plot(t_exact, x_exact[0], 'g-', t_exact, x_exact[1], 'b-', t_numerical, x_numerical[0], 'r--',
+             t_numerical, x_numerical[1], 'k--')
+    plt.grid(True)
+    plt.show()
+    plt.close()
---
+++ RungeKutta4/ode_826.py
@@ -0,0 +1,46 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import RungeKutta4 as rk4
+import RungeKutta4_cython as rk4_cython
+import time as time
+
+
+def ode_f(t, x):
+    return np.array([x[1] + 2 * np.exp(t), x[0] + t*t])
+
+
+def ode_exact_solution(t):
+    return np.array([np.exp(t) + np.exp(-t) + t * np.exp(t) - t * t - 2.0,
+                     np.exp(t) - np.exp(-t) + (t - 1.0) * np.exp(t) - 2.0 * t])
+
+
+if __name__ == '__main__':
+
+    a, b = -1.0, 1.0
+    grid_dot_num = 500
+    figsize = (15.0, 7.5)
+    t0, x0 = 0.0, np.array([0.0, -1.0])
+    h_initial = 0.1
+
+    t_exact = np.linspace(a, b, grid_dot_num)
+    x_exact = ode_exact_solution(t_exact)
+
+    tp = time.time()
+    t_numerical, x_numerical = rk4.solve_ode(t0, x0, a, b, ode_f, calc_eps=1e-8, h_initial=h_initial)
+    tp = time.time() - tp
+
+    tc = time.time()
+    t_numerical_cython, x_numerical_cython = rk4_cython.solve_ode(t0, x0, a, b, ode_f, calc_eps=1e-8,
+                                                                  h_initial=h_initial)
+    tc = time.time() - tc
+
+    print('||t1 - t2|| = %f\n||x1 - x2|| = %f\nTime python: %f\nTime cython: %f\n' %
+          (np.linalg.norm(t_numerical - t_numerical_cython), np.linalg.norm(x_numerical - x_numerical_cython),
+           tp, tc))
+
+    plt.figure(figsize=figsize)
+    plt.plot(t_exact, x_exact[0], 'g-', t_exact, x_exact[1], 'b-', t_numerical, x_numerical[0], 'r--',
+             t_numerical, x_numerical[1], 'k--')
+    plt.grid(True)
+    plt.show()
+    plt.close()
